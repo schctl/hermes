67ddc6387943fd217d98bbe61583de7b3acbe7b1
Sachin Cherian <sachinctl@protonmail.com>
2024-12-13 00:54:39 +0530

    node pub/sub handler and tests

--- src/link.rs
+++ src/link.rs
@@ -1,11 +1,10 @@
-use core::fmt::Debug;
-
+use heapless::spsc::Queue;
 use heapless::Vec;
 use postcard::accumulator::{CobsAccumulator, FeedResult};
 
 use crate::packet::Packet;
 
-pub trait Link: Debug {
+pub trait Link {
     fn read(&mut self) -> nb::Result<u8, ()>;
     fn write(&mut self, byte: u8) -> nb::Result<(), u8>;
 }
@@ -18,6 +17,42 @@ impl<const T: usize> Link for Vec<u8, T> {
 
     fn write(&mut self, byte: u8) -> nb::Result<(), u8> {
         self.push(byte).map_err(nb::Error::Other)
+    }
+}
+
+pub struct ChannelLink<'a, const T: usize, const N: usize> {
+    rx: heapless::spsc::Consumer<'a, u8, T>,
+    tx: heapless::spsc::Producer<'a, u8, N>,
+}
+
+impl<'a, const T: usize, const N: usize> ChannelLink<'a, T, N> {
+    pub fn new(
+        queue_1: &'a mut Queue<u8, T>,
+        queue_2: &'a mut Queue<u8, N>,
+    ) -> (ChannelLink<'a, T, N>, ChannelLink<'a, N, T>) {
+        let split_1 = queue_1.split();
+        let split_2 = queue_2.split();
+
+        (
+            ChannelLink {
+                rx: split_1.1,
+                tx: split_2.0,
+            },
+            ChannelLink {
+                rx: split_2.1,
+                tx: split_1.0,
+            },
+        )
+    }
+}
+
+impl<'a, const T: usize, const N: usize> Link for ChannelLink<'a, T, N> {
+    fn read(&mut self) -> nb::Result<u8, ()> {
+        self.rx.dequeue().ok_or(nb::Error::WouldBlock)
+    }
+
+    fn write(&mut self, byte: u8) -> nb::Result<(), u8> {
+        self.tx.enqueue(byte).map_err(|e| nb::Error::Other(e))
     }
 }
 
@@ -61,6 +96,7 @@ impl<'l> LinkedNode<'l> {
         self.read_buf = remaining.clone();
     }
 
+    // TODO: make this nb
     pub fn read_packet(&mut self, mut f: impl FnMut(Packet)) {
         while let Ok(byte) = self.link.read() {
             if self.read_buf.is_full() {
@@ -72,17 +108,41 @@ impl<'l> LinkedNode<'l> {
 
         self._accumulate(&mut f);
     }
+
+    pub fn write_packet(&mut self, packet: &Packet) {
+        let bytes = postcard::to_vec_cobs::<Packet, 256>(&packet).unwrap();
+
+        for byte in bytes {
+            self.link.write(byte).unwrap();
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use crate::node;
-    use crate::packet::{Message, Packet};
-    use crate::topic;
-
     use crate::packet::tests::TEST_PACKETS;
+    use crate::packet::Packet;
+
+    /// Test that channel link can correctly send and receive bytes.
+    #[test]
+    fn test_channel_link() {
+        // FIXME: channel driver queue size is kind of funky
+
+        let mut buffer_1 = Queue::<u8, 16>::new();
+        let mut buffer_2 = Queue::<u8, 16>::new();
+
+        let (mut link_1, mut link_2) = ChannelLink::new(&mut buffer_1, &mut buffer_2);
+
+        for byte in 0..12 {
+            link_1.write(byte).unwrap();
+        }
+
+        for byte in 0..12 {
+            assert_eq!(link_2.read().unwrap(), byte);
+        }
+    }
 
     /// Check that the internal accumulator can correctly handle receiving a stream of bytes
     /// and decode it into corresponding packets.
--- src/node.rs
+++ src/node.rs
@@ -1,6 +1,9 @@
+use heapless::FnvIndexSet;
 use slab::Slab;
 
 use crate::link::{Link, LinkedNode};
+use crate::packet::{self, Packet};
+use crate::topic;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Deserialize, serde::Serialize)]
 pub enum Id {
@@ -10,7 +13,9 @@ pub enum Id {
 
 pub struct Node<'l> {
     id: Id,
+    // FIXME: all this can probably be more memory efficient
     links: Slab<LinkedNode<'l>>,
+    subscriptions: FnvIndexSet<topic::Id, 16>,
 }
 
 impl<'l> Node<'l> {
@@ -18,6 +23,7 @@ impl<'l> Node<'l> {
         Self {
             id,
             links: Slab::with_capacity(4),
+            subscriptions: FnvIndexSet::new(),
         }
     }
 
@@ -27,5 +33,91 @@ impl<'l> Node<'l> {
 
     pub fn remove_link(&mut self, link: usize) -> LinkedNode<'l> {
         self.links.remove(link)
+    }
+
+    pub fn subscribe(&mut self, id: topic::Id) -> bool {
+        self.subscriptions.insert(id).is_ok()
+    }
+
+    pub fn unsubscribe(&mut self, id: topic::Id) -> bool {
+        self.subscriptions.remove(&id)
+    }
+
+    pub fn publish(&mut self, id: topic::Id, data: &[u8]) {
+        for (_, link) in self.links.iter_mut() {
+            link.write_packet(&Packet {
+                origin: self.id,
+                message: packet::Message::Publish(topic::Message { id, data }),
+            });
+        }
+    }
+
+    pub fn process_subscriptions(&mut self, mut f: impl FnMut(topic::Message)) {
+        for (_, link) in self.links.iter_mut() {
+            link.read_packet(|p| match p.message {
+                packet::Message::Publish(m) => {
+                    if self.subscriptions.contains(&m.id) {
+                        (f)(m);
+                    }
+                }
+            });
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use heapless::spsc::Queue;
+
+    use super::*;
+    use crate::link::ChannelLink;
+
+    const MESSAGES: [topic::Message; 4] = [
+        topic::Message {
+            id: 2,
+            data: &[0, 1, 2, 3],
+        },
+        topic::Message {
+            id: 2,
+            data: &[4, 5, 6, 7],
+        },
+        topic::Message {
+            id: 3,
+            data: &[8, 9, 10, 11],
+        },
+        topic::Message {
+            id: 2,
+            data: &[12, 13, 14, 15],
+        },
+    ];
+
+    #[test]
+    fn test_node_pub_sub() {
+        let mut buffer_1 = Queue::<u8, 128>::new();
+        let mut buffer_2 = Queue::<u8, 128>::new();
+
+        let (mut link_1, mut link_2) = ChannelLink::new(&mut buffer_1, &mut buffer_2);
+
+        let mut node_1 = Node::new(Id::Id(5));
+        node_1.add_link(&mut link_1);
+
+        let mut node_2 = Node::new(Id::Id(6));
+        node_2.add_link(&mut link_2);
+
+        node_2.subscribe(2);
+        node_2.subscribe(3);
+
+        for m in MESSAGES {
+            node_1.publish(m.id, m.data);
+        }
+
+        let mut idx = 0;
+
+        node_2.process_subscriptions(|m| {
+            assert_eq!(m, MESSAGES[idx]);
+            idx += 1;
+        });
+
+        assert_eq!(idx, MESSAGES.len());
     }
 }
